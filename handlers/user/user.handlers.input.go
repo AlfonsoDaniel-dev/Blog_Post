8fdfb14457f2469603660d082640745e632ac23b
alfonso <alfonso.dancerod@gmail.com>
2024-05-10 11:59:19 -0600

    Reesteucturacion del proyecto

--- handlers/user/user.handlers.input.go
+++
@@ -1,8 +0,0 @@
-package user
-
-import "github.com/TeenBanner/Inventory_system/models"
-
-type input interface {
-	CreateUser(user models.User) error
-	GetAllUsers() ([]models.User, error)
-}
--- models/model.user.admin.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type Admin struct {
-	User
-}
