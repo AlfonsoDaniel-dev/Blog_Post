2178cc6d91730ee0814116f6a8c9b68d17c90322
Alfonso <alfonso.dancerod@gmail.com>
2024-05-10 16:44:11 -0600

    Leer variables de entorno y conectarse a la base de datos

--- database/connection.go
+++ database/connection.go
@@ -1,1 +1,22 @@
 package database
+
+import (
+	"database/sql"
+	"sync"
+)
+
+var (
+	db   *sql.DB
+	once sync.Once
+	err  error
+)
+
+func CreateConnection(conStr string) {
+	once.Do(func() {
+		db, err = sql.Open("postgres", conStr)
+	})
+}
+
+func Pool() *sql.DB {
+	return db
+}
--- go.mod
+++ go.mod
@@ -8,3 +8,5 @@ require (
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.22.0
 )
+
+require github.com/joho/godotenv v1.5.1 // indirect
