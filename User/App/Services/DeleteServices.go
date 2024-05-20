79faf9defc5d64915eae91e5dcfbfe60fedbf479
Alfonso <alfonso.dancerod@gmail.com>
2024-05-19 19:11:49 -0600

    fix a Delete Post verficando que el post a eliminar existe

--- User/App/Services/DeleteServices.go
+++ User/App/Services/DeleteServices.go
@@ -9,7 +9,17 @@ func (S *Service) DeletePost(title, email string) error {
 		return errors.New("please provide a valid searchTitle or valid searchEmail")
 	}
 
-	err := S.UseCase.DeletePostByTitleAndEmail(title, email)
+	postid, err := S.UseCase.FindPostId(email, title)
+	if err != nil {
+		return errors.New("post id not found")
+	}
+
+	_, err = S.UseCase.FindPostById(postid)
+	if err != nil {
+		return errors.New("post not found")
+	}
+
+	err = S.UseCase.DeletePostByTitleAndEmail(title, email)
 	if err != nil {
 		return errors.New("somthing went wrong while deleting post")
 	}
